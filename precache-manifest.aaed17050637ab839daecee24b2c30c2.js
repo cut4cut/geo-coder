cfa2f604b5c13234d78046b89b096cc0ef02286d
vladp <vladp@ubuntu.ubuntu-domain>
2018-11-24 23:19:04 +0300

    Updates

--- precache-manifest.aaed17050637ab839daecee24b2c30c2.js
+++ precache-manifest.aaed17050637ab839daecee24b2c30c2.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/geo-coder/static/js/runtime~main.c39917fc.js"
   },
   {
-    "revision": "bf5382803d7692665f67",
-    "url": "/geo-coder/static/js/main.bf538280.chunk.js"
+    "revision": "266f78fa1df2f780dd86",
+    "url": "/geo-coder/static/js/main.266f78fa.chunk.js"
   },
   {
     "revision": "f74dfcb31409dc744522",
     "url": "/geo-coder/static/js/1.f74dfcb3.chunk.js"
   },
   {
-    "revision": "bf5382803d7692665f67",
-    "url": "/geo-coder/static/css/main.0186f27e.chunk.css"
+    "revision": "266f78fa1df2f780dd86",
+    "url": "/geo-coder/static/css/main.3ee0dce6.chunk.css"
   },
   {
     "revision": "f74dfcb31409dc744522",
     "url": "/geo-coder/static/css/1.966cfa8f.chunk.css"
   },
   {
-    "revision": "4412ccb6499616ef03d2b957f371ddb1",
+    "revision": "e91898549befc10207ef02b3e9fdb2ff",
     "url": "/geo-coder/index.html"
   }
 ];
